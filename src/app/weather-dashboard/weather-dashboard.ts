893924c781bcb9b70afe18346ea99b8b0fbd90e1
Susanne714 <sambale.su@gmail.com>
2025-09-16 17:08:36 +0200

    changed @Input to 'LocationResult', moved-in ngOnChanges() from current-weather

--- src/app/weather-dashboard/weather-dashboard.ts
+++ src/app/weather-dashboard/weather-dashboard.ts
@@ -1,10 +1,12 @@
-import { Component, Input, Output, EventEmitter, ViewChild } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 
 import { CurrentWeather } from '../current-weather/current-weather';
 import { WeatherForecast } from '../weather-forecast/weather-forecast';
+
+import { LocationResult } from '../services/weather';
 
 @Component({
   selector: 'app-weather-dashboard',
@@ -13,9 +15,18 @@ import { WeatherForecast } from '../weather-forecast/weather-forecast';
   styleUrls: ['./weather-dashboard.scss']
 })
 export class WeatherDashboard {
-  @Input() location: string = '';
+  // @Input() location: string = '';
+  @Input() location: LocationResult | null = null; //locationTest
   @Output() locationNameChange = new EventEmitter<string>();
   @ViewChild('currentWeather') currentWeather!: CurrentWeather;
+
+  // 👉 reagiert auf Änderungen von location //locationtest (Funktion neu eingefügt)
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['location']?.currentValue && this.currentWeather) {
+      const loc = this.location!;
+      this.currentWeather.loadWeather(loc.lat, loc.lon, loc.name);
+    }
+  }
 
   // Diese Methode wird von App aufgerufen (via ViewChild)
   getCurrentLocationWeather() {

13a1730734d3b93d840fb85bb83093c64a612feb
Susanne714 <sambale.su@gmail.com>
2025-09-18 18:23:44 +0200

    added footer component

---
+++ src/app/footer/footer.ts
@@ -0,0 +1,12 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  imports: [RouterLink],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss'
+})
+export class Footer {
+
+}

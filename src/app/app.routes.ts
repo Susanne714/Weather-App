549b42697bc02c8562407c8d63d96a7df81beb0c
Susanne714 <sambale.su@gmail.com>
2025-09-18 18:13:32 +0200

    set routing paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LegalNotice } from './shared/legal-notice/legal-notice';
+import { PrivacyPolicy } from './shared/privacy-policy/privacy-policy';
+import { DashboardWrapper } from './dashboard-wrapper/dashboard-wrapper';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: DashboardWrapper }, // Hauptseite = WeatherDashboard, DashboardWrapper als Transporter
+    { path: 'legalNotice', component: LegalNotice },
+    { path: 'privacyPolicy', component: PrivacyPolicy }
+];

e7843d76ebbaedfcaeb718cc67b467291931d52c
Susanne714 <sambale.su@gmail.com>
2025-09-18 18:20:51 +0200

    added routes import

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(App, {
   providers: [
     provideHttpClient(),
+    provideRouter(routes),
     ...appConfig.providers
   ]
 }).catch((err) => console.error(err));

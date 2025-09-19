dd3972ad9c0210fe96292f38090c8bf77585cb7b
Susanne714 <sambale.su@gmail.com>
2025-09-19 17:23:22 +0200

    meta tag mgmt

--- src/app/shared/privacy-policy/privacy-policy.ts
+++ src/app/shared/privacy-policy/privacy-policy.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-privacy-policy',
@@ -8,5 +9,9 @@ import { RouterLink } from '@angular/router';
   styleUrl: './privacy-policy.scss'
 })
 export class PrivacyPolicy {
+
+  constructor(private meta: Meta) {
+    this.meta.updateTag({ name: 'robots', content: 'noindex, follow' });
+  }
 
 }

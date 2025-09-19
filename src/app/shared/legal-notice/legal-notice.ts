fe82356d530fe97d5559d2d7233c8b16362a8af0
Susanne714 <sambale.su@gmail.com>
2025-09-19 17:20:57 +0200

    meta tag mgmt

--- src/app/shared/legal-notice/legal-notice.ts
+++ src/app/shared/legal-notice/legal-notice.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-legal-notice',
@@ -8,5 +9,9 @@ import { RouterLink } from '@angular/router';
   styleUrl: './legal-notice.scss'
 })
 export class LegalNotice {
+
+  constructor(private meta: Meta) {
+    this.meta.updateTag({ name: 'robots', content: 'noindex, follow' });
+  }
 
 }
